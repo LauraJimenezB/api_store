2cf95850755598f1f2d41db61a27f9364a0efe19
LauraJimenezB <laura271260@gmail.com>
2021-06-29 00:45:01 -0500

    env changes

--- src/attachments/config/attachments.config.ts
+++ src/attachments/config/attachments.config.ts
@@ -1,8 +1,8 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('attachment', () => ({
-  accessKeyId: 'AKIAXA4D74BJNUZWNHIZ',
-  secretAccessKey: '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',
+  accessKeyId: AWS_ACCESS_KEY_ID,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
   bucket: process.env.AWS_BUCKET,
   expirationTime: +process.env.PRESIGNED_EXPIRES_IN,
