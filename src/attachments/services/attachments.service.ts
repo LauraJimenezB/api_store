7eb2fd6c16485b7a173f6d4c24e79761c8a4b932
LauraJimenezB <laura271260@gmail.com>
2021-06-29 08:56:04 -0500

    attachments

--- src/attachments/services/attachments.service.ts
+++ src/attachments/services/attachments.service.ts
@@ -89,15 +89,6 @@ export class AttachmentsService {
     });
     return plainToClass(AttachmentDto, { signedUrl, ...attachment });
   } 
-  /* async uploadImages(type: string, bookId: number): Promise<{ url: string }> {
-    //type = 'image/jpeg';
-    const { key, url } = this.preSignedPutURL();
-    await this.prismaService.attachment.create({
-      data: { key, contentType: type, bookId },
-    });
-    console.log(this.configService.secretAccessKey);
-    return { url };
-  } */
 
   async getImages(bookId: number): Promise<AttachmentDto[]> {
     const attachments = await this.prismaService.attachment.findMany({
@@ -115,33 +106,4 @@ export class AttachmentsService {
       );
     }); */
   }
-  /* 
-  
-  async uploadImages(dataBuffer: Buffer, bookId: number, filename: string) {
-    const s3 = new S3();
-    const uploadResult = await s3
-      .upload({
-        Bucket: this.configService.bucket,
-        Body: dataBuffer,
-        Key: `${uuid()}-${filename}`,
-      })
-      .promise();
-
-    const newFile = this.prismaService.attachment.create({
-      data: {
-        key: uploadResult.Key,
-        bookId,
-      },
-    });
-    return newFile;
-  }
-
-  public async generatePresignedUrl(key: string) {
-    const s3 = new S3();
-
-    return s3.getSignedUrlPromise('getObject', {
-      Bucket: this.configService.bucket,
-      Key: key,
-    });
-  } */
 }
